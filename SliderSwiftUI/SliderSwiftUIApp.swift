67d28df2653edb134d7919dfa0ccc3f73e3053da
Isa <isa.arbuhov@mail.ru>
2022-12-25 20:56:03 +0300

    Initial Commit

---
+++ SliderSwiftUI/SliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderSwiftUIApp.swift
+//  SliderSwiftUI
+//
+//  Created by Иса on 25.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
